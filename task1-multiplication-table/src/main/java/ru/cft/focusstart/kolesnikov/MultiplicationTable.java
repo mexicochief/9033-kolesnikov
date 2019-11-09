67f88a2c8ca70b4d9e66b47826939183857a3316
thisDiamondIsUnbrekable <44873983+mexicochief@users.noreply.github.com>
2019-11-09 13:29:02 +0700

    refactored code and added test

--- task1-multiplication-table/src/main/java/ru/cft/focusstart/kolesnikov/MultiplicationTable.java
+++ task1-multiplication-table/src/main/java/ru/cft/focusstart/kolesnikov/MultiplicationTable.java
@@ -1,67 +1,70 @@
 package ru.cft.focusstart.kolesnikov;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintStream;
+
 
 public class MultiplicationTable {
+
     public static void main(String[] args) {
-        int tableSize = getTableSize();
-        int cellSize = getCellSize(tableSize);
+        int tableSize = getTableSize(new BufferedReader(new InputStreamReader(System.in)));
 
-        buildTable(tableSize, cellSize);
+        String table = buildTableAsString(tableSize);
+        writeToConsole(System.out, table);
     }
 
-
-    private static int getTableSize() {
-
+    public static int getTableSize(BufferedReader bufferedReader) {
+        int tableSize;
         try {
-            return readFromConsole();
-        } catch (InputMismatchException e) {
-            System.out.println("Должно быть целое положительное число");
-            return getTableSize();
+            tableSize = readFromConsole(bufferedReader);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Wrong value, must be integer number with range [0,32]");
+        } catch (IOException e) {
+            tableSize = getTableSize(bufferedReader);
         }
-
+        return tableSize;
     }
 
-    private static int readFromConsole() {
-        Scanner scanner = new Scanner(System.in);
-        int size = scanner.nextInt();
-        if (size < 0) throw new InputMismatchException();
+    private static int readFromConsole(BufferedReader bufferedReader) throws IOException, NumberFormatException {
+        System.out.println("Print integer number with range [0,32]");
+        int size = Integer.parseInt(bufferedReader.readLine());
+
+        if (0 > size || size > 32) {
+            throw new IllegalArgumentException("Wrong value, must be integer number with range [0,32]");
+        }
         return size;
     }
 
+    public static String buildTableAsString(int tableSize) {
+        StringBuilder resultTable = new StringBuilder();
+        int cellSize = getCellSize(tableSize);
+        String separateLine = createLine(tableSize, cellSize);
 
-    private static int getCellSize(int size) {
-        String temp = String.valueOf(size * size);
+        for (int i = 1; i <= tableSize; i++) {
+            for (int j = 1; j <= tableSize; j++) {
+                int tableElement = j * i;
+                String resultString = convertToCell(tableElement, cellSize);
+                resultTable.append(resultString);
+                if (j != tableSize) {
+                    resultTable.append("|");
+                }
+            }
+            if (i != tableSize) {
+                resultTable.append("\n");
+                resultTable.append(separateLine);
+                resultTable.append("\n");
+            }
+        }
+        return resultTable.toString();
+    }
+
+    private static int getCellSize(int tableSize) {
+        String temp = String.valueOf(tableSize * tableSize);
         return temp.length();
     }
-
-
-    private static void buildTable(int tablesize, int sqaresize) {
-        for (int i = 1; i <= tablesize; i++) {
-            for (int j = 1; j < tablesize; j++) {
-                int tableElement = j * i;
-                String resultString = convertToCell(tableElement, sqaresize);
-                System.out.printf("%s%s",resultString, "|");
-            }
-            int lastElement = tablesize * i;
-            System.out.println(convertToCell(lastElement, sqaresize)); // добавить последнюю клетку(без |)
-            if (i != tablesize) System.out.println(createLine(tablesize, sqaresize)); // вывести разделитель
-
-        }
-
-    }
-
-
-    private static String convertToCell(int number, int maxNumberLen) {
-        String res = String.valueOf(number);
-        int temp = maxNumberLen;
-        String str;
-        str = String.format("%" + temp + "s", res);
-
-        return str;
-    }
-
 
     private static String createLine(int size, int len) {
         StringBuilder stringBuilder = new StringBuilder();
@@ -72,11 +75,19 @@ public class MultiplicationTable {
             stringBuilder.append(lineElement);
             stringBuilder.append("+");
         }
-        stringBuilder.append(lineElement); // добавить последний элемент строки(без + )
-
+        stringBuilder.append(lineElement);
         return String.valueOf(stringBuilder);
-
     }
+
+    private static String convertToCell(int number, int maxNumberLen) {
+        String res = String.valueOf(number);
+        return String.format("%" + maxNumberLen + "s", res);
+    }
+
+    public static void writeToConsole(PrintStream printStream, String str) {
+        printStream.println(str);
+    }
+
 
 }
 
---
+++ task1-multiplication-table/src/test/java/ru/cft/focusstart/kolesnikov/MultiplicationTableTest.java
@@ -0,0 +1,74 @@
+package ru.cft.focusstart.kolesnikov;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.*;
+import static ru.cft.focusstart.kolesnikov.MultiplicationTable.*;
+
+public class MultiplicationTableTest {
+
+    private String strExpected1 = " 1| 2| 3| 4| 5\n" +
+            "--+--+--+--+--\n" +
+            " 2| 4| 6| 8|10\n" +
+            "--+--+--+--+--\n" +
+            " 3| 6| 9|12|15\n" +
+            "--+--+--+--+--\n" +
+            " 4| 8|12|16|20\n" +
+            "--+--+--+--+--\n" +
+            " 5|10|15|20|25";
+
+    private final PrintStream printStream = mock(PrintStream.class);
+    private final BufferedReader bufferedReader = mock(BufferedReader.class);
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void testGetTableSize() throws IOException {
+        when(bufferedReader.readLine()).thenReturn("5");
+        assertEquals(5, getTableSize(bufferedReader));
+        verify(bufferedReader).readLine();
+    }
+
+    @Test
+    public void testGetTableSizeWithIllegalArguments() throws IOException {
+        when(bufferedReader.readLine()).thenReturn("A");
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Wrong value, must be integer number with range [0,32]");
+        getTableSize(bufferedReader);
+    }
+
+    @Test
+    public void testGetTableSizeWithWrongRange() throws IOException {
+        when(bufferedReader.readLine()).thenReturn("33");
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Wrong value, must be integer number with range [0,32]");
+        getTableSize(bufferedReader);
+    }
+
+    @Test
+    public void testGetTableSizeWithIOException() throws IOException {
+        when(bufferedReader.readLine()).thenThrow(IOException.class).thenReturn("5");
+        getTableSize(bufferedReader);
+        verify(bufferedReader, times(2)).readLine();
+    }
+
+    @Test
+    public void testBuildTableAsString() {
+        assertEquals(strExpected1, buildTableAsString(5));
+    }
+
+    @Test
+    public void testWriteToConsole() {
+        writeToConsole(printStream, strExpected1);
+        verify(printStream).println(strExpected1);
+    }
+
+}
