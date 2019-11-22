2a791f1816d6a92deabc88587dfcf3a50c78b53e
thisDiamondIsUnbrekable <44873983+mexicochief@users.noreply.github.com>
2019-11-23 01:09:37 +0700

    Task2: Изменен сособ подсчета количества строк в файле

--- task2/src/main/java/ru/cft/focusstart/kolesnikov/reading/FigureParametersReader.java
+++ task2/src/main/java/ru/cft/focusstart/kolesnikov/reading/FigureParametersReader.java
@@ -4,57 +4,45 @@ import ru.cft.focusstart.kolesnikov.figure.figuretype.FigureType;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class FigureParametersReader {
 
     public static FigureParameters getFigureParameters(String filename) throws IOException {
         File file = new File(filename);
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-             Scanner scanner = new Scanner(file)) {
-            FigureType figureType = readFigureType(bufferedReader);
-            checkQuantityLineInFile(scanner, figureType);
+        try (Scanner scanner = new Scanner(file)) {
+            FigureType figureType = readFigureType(scanner);
             return new FigureParameters(figureType,
-                    readFigureParameters(bufferedReader, figureType));
+                    readFigureParameters(scanner, figureType));
         } catch (FileNotFoundException e) {
             throw new FileNotFoundException("Указанный файл не найден");
         }
     }
 
-    private static FigureType readFigureType(BufferedReader bufferedReader) throws IOException {
-        String str = bufferedReader.readLine();
+    private static FigureType readFigureType(Scanner scanner) {
+        String str = scanner.nextLine();
         return FigureType.valueOf(str);
     }
 
-    private static void checkQuantityLineInFile(Scanner scanner, FigureType figureType) throws IOException {
-        if (quantityLineInFile(scanner, figureType) != figureType.quantityParameters) {
-            throw new IOException("Некоректное число строк в файле");
-        }
-    }
-
-    private static int quantityLineInFile(Scanner scanner, FigureType figureType) {
-        int count = 0;
-        while (scanner.hasNext() && figureType.quantityParameters >= count) {
-            scanner.nextLine();
-            count++;
-        }
-        return count;
-    }
-
-    private static ArrayList<Double> readFigureParameters(BufferedReader bufferedReader, FigureType figureType) throws IOException {
+    private static ArrayList<Double> readFigureParameters(Scanner scanner,
+                                                          FigureType figureType) throws IOException {
         ArrayList<Double> parameters = new ArrayList<>();
         for (int i = 1; i < figureType.quantityParameters; i++) {
-            parameters.add(readCorrectParameter(bufferedReader));
+            parameters.add(readCorrectParameter(scanner));
+        }
+        if (scanner.hasNext()) {
+            throw new IOException("Слишком много арументов");
         }
         return parameters;
     }
 
-    private static double readCorrectParameter(BufferedReader bufferedReader) throws IOException {
+    private static double readCorrectParameter(Scanner scanner) {
         double value;
         try {
-            value = Double.valueOf(bufferedReader.readLine());
-        } catch (NumberFormatException e) {
-            throw new NumberFormatException("Неверный символ в файле или пустая строка");
+            value = scanner.nextDouble();
+        } catch (NoSuchElementException e) {
+            throw new NoSuchElementException("Неверный символ в файле или пустая строка");
         }
         if (value < 0) {
             throw new IllegalArgumentException("отрицательное значение в файле");
