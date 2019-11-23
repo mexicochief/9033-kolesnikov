af81c5998cabd4a0880157de133290cb0908870e
thisDiamondIsUnbrekable <44873983+mexicochief@users.noreply.github.com>
2019-11-23 13:56:01 +0700

    Task3: убранна ненужная синхронизация в Product

--- Task3/src/main/java/ru/cft/focusstrat/kolesnikov/Product.java
+++ Task3/src/main/java/ru/cft/focusstrat/kolesnikov/Product.java
@@ -1,16 +1,14 @@
 package ru.cft.focusstrat.kolesnikov;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class Product {
-    private static AtomicInteger currentProductId = new AtomicInteger(1);
+    private static int instanceCount = 0;
     private int id;
 
     Product(){
-        id = currentProductId.getAndIncrement();
+        id = instanceCount++;
     }
 
-    synchronized int getId() { // тут не уверен насчёт модификатора
+    int getId() {
         return id;
     }
 }
